0a3c319854ad10f3f6e6ff369afe9e31ef9e94c1
Tim Hartmann <tfhartmann@gmail.com>
2013-07-09 21:58:49 -0400

    Updated packages
    Moved packages to the common module

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,9 +6,4 @@ node default {
   class { 'mysql::java': }
   class { 'mysql::python': }
   class { 'mysql::ruby': }
-  
-  # These should be in a common HUIT Module I think
-  package { 'git'         : ensure => present, }
-  package { 'vim-enhanced': ensure => present, }
-  package { 'emacs-nox'   : ensure => present, }
 }
