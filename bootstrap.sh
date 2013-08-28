be4c42564787e303ccb91bbb6f441152853cb78d
Daniel Silverman <me@agperson.com>
2013-08-28 14:50:59 -0400

    Simplifying and speeding up bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-
-yum -y update
-yum install -y -q git
-
-if [[ "$(gem query -i -n r10k)" == "false" ]]; then
-  gem install --no-rdoc --no-ri r10k
-fi
-
-cd /vagrant && r10k puppetfile install
-
-# Put in a brief wait before running the Puppet provisioner to clear yum activity
-sleep 5
+[[ $(rpm -qa | grep ^git-) ]] || (yum install -y -q git && sleep 5)
+[[ $(gem query -i -n r10k) ]] || gem install --no-rdoc --no-ri r10k
+cd /vagrant && r10k -v info puppetfile install
