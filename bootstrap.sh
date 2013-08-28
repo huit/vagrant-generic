959985cc630fba59ab93380287f56816b63637ef
Daniel Silverman <me@agperson.com>
2013-08-28 15:35:24 -0400

    Fixing busted bootstrap!

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 [[ $(rpm -qa | grep ^git-) ]] || (yum install -y -q git && sleep 5)
-[[ $(gem query -i -n r10k) ]] || gem install --no-rdoc --no-ri r10k
+[[ "$(gem query -i -n r10k)" == "true" ]] || gem install --no-rdoc --no-ri r10k
 cd /vagrant && r10k -v info puppetfile install
