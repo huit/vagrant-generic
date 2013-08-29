2f42308228df58d669832bf7c6142b00e74570b0
Tim Hartmann <tfhartmann@gmail.com>
2013-08-29 13:48:55 -0400

    Working MediaWiki, not setup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,34 @@
 node default {
   stage { 'pre': before => Stage['main'] }
 
-  class { 'apache': default_ssl_vhost => true }
   class { 'epel': stage => 'pre' }
   class { 'common': }
 
-  class { 'mysql': }
-  class { 'mysql::server': }
-  class { 'mysql::java': }
-  class { 'mysql::python': }
-  class { 'mysql::ruby': }
+  #class { 'mysql': }
+  #class { 'mysql::server': }
+  #class { 'mysql::java': }
+  #class { 'mysql::python': }
+  #class { 'mysql::ruby': }
+  #class { 'apache': default_ssl_vhost => true }
+
+# PoCVagrant (DB Instance Identifier 
+# pocVagrantUser
+# _long_password
+# db name : mediawiki
+
+  class { 'mediawiki':
+    server_name      => $::ec2_public_hostname,
+    admin_email      => 'admin@myawesomesite.com',
+    db_root_password => '_long_password',
+    db_server        => 'pocvagrant.cf2rixq5q6pn.us-east-1.rds.amazonaws.com',
+    doc_root         => '/var/www/html',
+    max_memory       => '1024'
+  }
+  mediawiki::instance { 'my_wiki1':
+    db_password => 'really_long_password',
+    db_name     => 'mediawiki',
+    db_user     => 'pocVagrantUser',
+    port        => '80',
+    ensure      => 'present'
+  }
 }
