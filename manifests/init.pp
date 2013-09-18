11b4fb4b3b4100bd8bce380c482bf6e48393f3df
Tim Hartmann <tfhartmann@gmail.com>
2013-09-18 14:13:40 -0400

    Update to init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,18 +3,5 @@ node default {
   class { 'epel': stage => 'pre' }
   class { 'common': }
 
-  class { 'mysql': }
-  class { 'mysql::server': }
-  class { 'mysql::java': }
-  class { 'mysql::python': }
-  class { 'mysql::ruby': }
-  # Redis Should be it's own module
-  package { 'redis': ensure=> installed, }
-  class { 'hubot':
-    repo_url => '',
-    repo_ref => 'master',
-    env_vars => { 'HUBOT_HIPCHAT_JID' => '',
-                  'HUBOT_HIPCHAT_PASSWORD'   => '',
-                  'HUBOT_HIPCHAT_ROOMS' => '' },
-  }
+  class { 'hubot': }
 }
