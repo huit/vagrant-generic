7a0fab04b5c1f4717ee77814ac3a98461aca04ec
Tim Hartmann <tfhartmann@gmail.com>
2014-01-24 17:34:45 -0500

    updated

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,12 @@
 node default {
   class { 'common': }
   class { 'puppetlabs_yum': }
-  class { 'foreman':
-    db_type            => 'mysql',
-    configure_scl_repo => false,
+  class { 'epel': }
+  package { 'foreman-release':
+    provider => 'rpm',
+    source   => 'http://yum.theforeman.org/releases/1.3/el6/x86_64/foreman-release.rpm',
   }
-  class { 'puppet': server => true }
+  package { 'foreman-installer':
+    require => Package['foreman-release'],
+  }
 }
